358674978f241273bfc0b85417251d1ef0de5d5f
LightAkabane <145754116+LightAkabane@users.noreply.github.com>
2025-11-23 18:18:43 +0100

    Maj utilisation full webgpu

--- get_osnet_onnx.py
+++ get_osnet_onnx.py
@@ -1,29 +1,25 @@
 import torch
 import torchreid
 
-# 1. Charger un OSNet léger pré-entraîné
 model = torchreid.models.build_model(
-    name='osnet_x0_25',   # version light
+    name='resnet18',
     num_classes=1000,
-    pretrained=True       # charge des poids pré-entraînés ReID
+    pretrained=True
 )
 
-# On veut le modèle en mode "features", pas en classification
-model.classifier = torch.nn.Identity()
+# On supprime la tête de classification
+model.fc = torch.nn.Identity()
 model.eval()
 
-# 2. Input factice (B, C, H, W) = (1, 3, 256, 128)
 dummy = torch.randn(1, 3, 256, 128)
 
-# 3. Export ONNX
 torch.onnx.export(
     model,
     dummy,
-    "osnet_reid.onnx",
+    "resnet18_reid.onnx",
     input_names=["input"],
     output_names=["feat"],
-    opset_version=12,
-    do_constant_folding=True
+    opset_version=16,
+    do_constant_folding=True,
+    dynamic_axes={"input": {0: "batch"}, "feat": {0: "batch"}}
 )
-
-print("Export OK -> osnet_reid.onnx")
